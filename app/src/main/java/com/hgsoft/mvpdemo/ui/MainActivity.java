8d25558a1ad4695c1b0b5a165656851de6ef303e
ydp <yudapei@gmail.com>
2017-07-26 09:48:41 +0800

    1、加入butterknife8.7.0

--- app/src/main/java/com/hgsoft/mvpdemo/ui/MainActivity.java
+++ app/src/main/java/com/hgsoft/mvpdemo/ui/MainActivity.java
@@ -3,15 +3,26 @@ package com.hgsoft.mvpdemo.ui;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.hgsoft.mvpdemo.R;
 import com.hgsoft.mvpdemo.presenter.MainActivityPresenter;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
-public class MainActivity extends BaseActivity<MainActivityPresenter> implements View.OnClickListener {
+public class MainActivity extends BaseActivity<MainActivityPresenter>{
+
+    @BindView(R.id.textview)
+    TextView mTextview;
+    @BindView(R.id.button)
+    Button mButton;
+    @BindView(R.id.fl_container)
+    FrameLayout mFlContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +30,7 @@ public class MainActivity extends BaseActivity<MainActivityPresenter> implements
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        findViewById(R.id.button).setOnClickListener(this);
+//        findViewById(R.id.button).setOnClickListener(this);
         initFragment();
     }
 
@@ -30,10 +41,10 @@ public class MainActivity extends BaseActivity<MainActivityPresenter> implements
     }
 
     public void showStudentInfo(String info) {
-        ((TextView) findViewById(R.id.textview)).setText("activity:" + info);
+        mTextview.setText("activity:" + info);
     }
 
-    @Override
+    @OnClick(R.id.button)
     public void onClick(View view) {
         System.out.println("buttonOnclick");
         getPresenter().doSomeThing();
